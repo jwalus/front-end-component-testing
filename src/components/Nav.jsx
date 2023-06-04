4ca2277a400cf91370fa52f5857c7061b4f6dfc6
jwalus <walusjacob@gmail.com>
2023-06-03 19:29:29 -0500

    messing with nav bar stufff

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,93 @@
+import { useState, useEffect, useRef } from 'react';
 import { logo } from '../assets';
-import { GiHamburgerMenu } from 'react-icons/gi'
-
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const Nav = () => {
+  const Links = [
+    { name: 'Home', link: '/' },
+    { name: 'About', link: '/' },
+    { name: 'Products', link: '/' },
+    { name: 'Help', link: '/' },
+  ];
+
+  const [showMobileNav, setShowMobileNav] = useState(false);
+  const mobileNavRef = useRef(null);
+
+  const toggleMobileNav = () => {
+    setShowMobileNav(prevShowMobileNav => !prevShowMobileNav);
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = event => {
+      if (
+        mobileNavRef.current &&
+        !mobileNavRef.current.contains(event.target) &&
+        !event.target.classList.contains('GiHamburgerMenu') &&
+        showMobileNav
+      ) {
+        setShowMobileNav(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [showMobileNav]);
+
   return (
-    <div className='pt-4 pb-4 xm:mx-4 xl:mx-56 xxl:mx-64'>
-      <div className='flex w-full justify-between items-center'>
+    <div className="pt-4 pb-4 xm:mx-4 xl:mx-56 xxl:mx-64">
+      <div className="flex w-full justify-between items-center">
         <div>
-          <img src={logo} alt="logo" className='xm:w-[90px] xl:w-[130px]' />
+          <img
+            src={logo}
+            alt="logo"
+            className="xm:w-[90px] xl:w-[130px] cursor-pointer"
+          />
         </div>
-        <div className='md:flex hidden items-center'>
-          <ul className='flex gap-12 text-white font-semibold'>
-            <li className='hover:text-green-300'>Home</li>
-            <li className='hover:text-green-300'>About</li>
-            <li className='hover:text-green-300'>Products</li>
-            <li className='hover:text-green-300'>Help</li>
+        {/* Desktop Navigation */}
+        <div>
+          <ul className="text-white font-semibold md:gap-12 md:flex hidden md:items-center">
+            {Links.map(Link => (
+              <li
+                key={Link.name}
+                className="hover:text-green-300 hover:scale-105"
+              >
+                <a href={Link.link}>{Link.name}</a>
+              </li>
+            ))}
           </ul>
         </div>
-        <div className='flex items-center gap-4'>
-          <GiHamburgerMenu color="white" size={18} className='md:hidden flex'/>
-          <button className='text-white md:px-5 md:py-2 xm:px-3 xm:py-1 bg-gradient-to-r from-teal-400 to-blue-600 rounded-full font-semibold'>Login</button>
+        <div className="flex items-center gap-4">
+          <GiHamburgerMenu
+            color="white"
+            size={18}
+            className="md:hidden block GiHamburgerMenu"
+            onClick={toggleMobileNav}
+          />
+          <button className="text-white md:px-5 md:py-2 xm:px-3 xm:py-1 bg-gradient-to-r from-teal-400 to-blue-600 rounded-full font-semibold hover:scale-105">
+            Login
+          </button>
         </div>
       </div>
+      {/* Mobile Navigation */}
+      <div ref={mobileNavRef}>
+        {showMobileNav && (
+          <ul className="text-white font-semibold absolute right-0 top-12 bg-blue-900 w-full p-4 flex flex-col text-center leading-10">
+            {Links.map(Link => (
+              <li
+                key={Link.name}
+                className="hover:text-green-300 hover:scale-105"
+              >
+                <a href={Link.link}>{Link.name}</a>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Nav;
